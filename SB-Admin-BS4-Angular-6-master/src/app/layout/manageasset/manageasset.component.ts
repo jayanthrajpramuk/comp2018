b937cc738b6984c8e9b50d129a828ec3db22bd3a
Sharma Chetan <chetan.sharma@volvo.com>
2018-07-14 11:38:40 +0530

    asset management

--- SB-Admin-BS4-Angular-6-master/src/app/layout/manageasset/manageasset.component.ts
+++ SB-Admin-BS4-Angular-6-master/src/app/layout/manageasset/manageasset.component.ts
@@ -1,18 +1,55 @@
 import { Component, OnInit } from '@angular/core';
 import { routerTransition } from '../../router.animations';
 
+
+export interface Icommon {
+value: string;
+viewValue: string;
+} 
+
 @Component({
     selector: 'app-charts',
     templateUrl: './manageasset.component.html',
     styleUrls: ['./manageasset.component.scss'],
     animations: [routerTransition()]
 })
+
+
 export class ManageassetComponent implements OnInit {
     // bar chart
     public barChartOptions: any = {
         scaleShowVerticalLines: false,
         responsive: true
     };
+
+ selectedAssemblyValue: string;
+ selectedAssetAssemblyValue: string;
+ selectedAssetZoneValue: string;
+ selectedAssetValue: string;
+ selectedCalibrationValue: string;
+ selectedPrintChoiceValue: string;
+
+assemblyLines:Icommon[] = [
+{value: 'Line1', viewValue: 'Line 1'},
+{value: 'Line2', viewValue: 'Line 2'},
+{value: 'Line3', viewValue: 'Line 3'}
+]; 
+
+zones: Icommon[] = [
+    {value: 'Zone1', viewValue: 'Zone 1'},
+{value: 'Zone2', viewValue: 'Zone 2'},
+{value: 'Zone3', viewValue: 'Zone 3'}
+];
+
+printChoice: Icommon[]=[
+    {value:'zone',viewValue:'Zone'},
+    {value:'asset',viewValue:'Assets'}
+];
+assets: Icommon[] = [
+    {value: 'Wrench', viewValue: 'Wrench'},
+{value: 'Sling', viewValue: 'Sling'},
+];
+
     public barChartLabels: string[] = [
         '2006',
         '2007',
